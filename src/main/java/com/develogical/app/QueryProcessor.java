febdd349af7af0879e5b869d1a5b18f0e8865189
gsabirmalik <gsabirmalik@hotmail.com>
2016-07-11 16:53:37 +0100

    Added Allama Iqbal to the list

--- src/main/java/com/develogical/app/QueryProcessor.java
+++ src/main/java/com/develogical/app/QueryProcessor.java
@@ -3,10 +3,16 @@ package com.develogical.app;
 public class QueryProcessor {
 
     public String process(String query) {
-        if (query.toLowerCase().contains("shakespeare")) {
+        query = query.toLowerCase();
+
+        if (query.contains("shakespeare")) {
             return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
+        }
+        else if (query.contains("allama iqbal")) {
+            return "Allama Iqbal (09 November 1977 - 21 April 1938) was " +
+                    "a Pakistano Poet known to be the poest of East.";
         }
         return "";
     }
